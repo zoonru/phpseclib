6589b9bc00a42a2d35d504c2d605f94027e02158
spirin <i.v.spirin@gmail.com>
2019-08-30 21:24:29 +0300

    redis seeder

--- phpseclib/Crypt/Random.php
+++ phpseclib/Crypt/Random.php
@@ -132,53 +132,7 @@ class Random
         // everyone's http requests.
         static $crypto = false, $v;
         if ($crypto === false) {
-            // save old session data
-            $old_session_id = session_id();
-            $old_use_cookies = ini_get('session.use_cookies');
-            $old_session_cache_limiter = session_cache_limiter();
-            $_OLD_SESSION = isset($_SESSION) ? $_SESSION : false;
-            if ($old_session_id != '') {
-                session_write_close();
-            }
-
-            session_id(1);
-            ini_set('session.use_cookies', 0);
-            session_cache_limiter('');
-            session_start();
-
-            $v = $seed = $_SESSION['seed'] = pack('H*', sha1(
-                (isset($_SERVER) ? phpseclib_safe_serialize($_SERVER) : '') .
-                (isset($_POST) ? phpseclib_safe_serialize($_POST) : '') .
-                (isset($_GET) ? phpseclib_safe_serialize($_GET) : '') .
-                (isset($_COOKIE) ? phpseclib_safe_serialize($_COOKIE) : '') .
-                // as of PHP 8.1 $GLOBALS can't be accessed by reference, which eliminates
-                // the need for phpseclib_safe_serialize. see https://wiki.php.net/rfc/restrict_globals_usage
-                // for more info
-                (version_compare(PHP_VERSION, '8.1.0', '>=') ? serialize($GLOBALS) : phpseclib_safe_serialize($GLOBALS)) .
-                phpseclib_safe_serialize($_SESSION) .
-                phpseclib_safe_serialize($_OLD_SESSION)
-            ));
-            if (!isset($_SESSION['count'])) {
-                $_SESSION['count'] = 0;
-            }
-            $_SESSION['count']++;
-
-            session_write_close();
-
-            // restore old session data
-            if ($old_session_id != '') {
-                session_id($old_session_id);
-                session_start();
-                ini_set('session.use_cookies', $old_use_cookies);
-                session_cache_limiter($old_session_cache_limiter);
-            } else {
-                if ($_OLD_SESSION !== false) {
-                    $_SESSION = $_OLD_SESSION;
-                    unset($_OLD_SESSION);
-                } else {
-                    unset($_SESSION);
-                }
-            }
+            $v = $seed = Seeder::getSeed();
 
             // in SSH2 a shared secret and an exchange hash are generated through the key exchange process.
             // the IV client to server is the hash of that "nonce" with the letter A and for the encryption key it's the letter C.
